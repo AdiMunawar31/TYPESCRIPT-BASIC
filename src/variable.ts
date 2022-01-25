c8d4a7d7682f4e06ef9fd4bbfd92aafb9696696c
d2ycode <munawar.adi31@gmail.com>
2022-01-25 21:54:27 +0700

    Learn array and tuples in Typescript

---
+++ src/variable.ts
@@ -0,0 +1,12 @@
+// Learn Variabel in Typescript
+let myName: string = "Adi Munawar";
+let age: number = 20;
+let isActive: boolean = true;
+let all: any = true || 31 || "Adi";
+
+myName = "This is String";
+console.log(myName);
+console.log(age);
+console.log(isActive);
+console.log(all);
+
