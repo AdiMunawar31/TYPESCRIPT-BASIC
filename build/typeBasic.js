866fe9afe959594738df3f2772a821694915deeb
d2ycode <munawar.adi31@gmail.com>
2022-01-25 21:32:31 +0700

    learn string, number, boolean, any in ts

--- build/typeBasic.js
+++ build/typeBasic.js
@@ -1,5 +1,11 @@
 "use strict";
 // Learn Variabel in Typescript
 let myName = "Adi Munawar";
+let age = 20;
+let isActive = true;
+let all = true || 31 || "Adi";
 myName = "This is String";
 console.log(myName);
+console.log(age);
+console.log(isActive);
+console.log(all);
