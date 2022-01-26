873396db77275579d88ad3552bf0297a13ba6f13
d2ycode <munawar.adi31@gmail.com>
2022-01-26 08:58:41 +0700

    add file enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,19 @@
+// Enum
+
+// numeric enums
+enum Month {
+    JAN = 1, // default 0
+    FEB,
+    MAR,
+    APR,
+    MAY
+}
+
+// string enum
+enum Months {
+    JAN = "January",
+    FEB = "February",
+    MAR = "March",
+    APR = "April",
+    MAY = "May"
+}
