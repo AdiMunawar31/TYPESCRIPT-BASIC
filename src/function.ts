0b4d07ee1908d3f01b22746aaa2452ad4b44d073
d2ycode <munawar.adi31@gmail.com>
2022-01-26 09:21:34 +0700

    add file function.ts

---
+++ src/function.ts
@@ -0,0 +1,17 @@
+// Tipe data pada balikan function
+
+// with argument type
+function getName(name: string): string {
+    return name;
+}
+
+function getAge(): number {
+    return 20;
+}
+
+function printName(): void {
+    console.log("Print Name");
+    
+}
+
+console.log(getName("Adi Munawar"));
